31cf50016c7d622cb8f71ca8f1def61ad5f44a52
Innocent Udeogu <innocentudeh@gmail.com>
2021-03-16 12:42:46 +0100

    Update update.rb

--- core/app/services/spree/checkout/update.rb
+++ core/app/services/spree/checkout/update.rb
@@ -27,8 +27,9 @@ module Spree
       def selected_shipping_rate_present?(params)
         shipments_attributes = params.dig(:order, :shipments_attributes)
         return false unless shipments_attributes
-
-        shipments_attributes.to_unsafe_h.any? { |s| s.dig(:selected_shipping_rate_id) }
+        
+        shipments_attributes.to_unsafe_h = shipments_attributes
+        shipments_attributes.any? { |s| s.dig(:selected_shipping_rate_id) }
       end
 
       def replace_country_iso_with_id(params, address_kind = 'ship')
